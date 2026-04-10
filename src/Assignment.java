af94e1c9b49b0ae199deeb204762346746212c25
kaylaato <kaylalistenstomusic@gmail.com>
2026-04-10 17:44:04 +0000

    progress 4/10

---
+++ src/Assignment.java
@@ -0,0 +1,19 @@
+public class Assignment {
+    // INSTANCE VARIABLES
+    String name;
+    String dueDate;
+    int priority;
+    boolean isComplicated;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDueDate(){
+        return dueDate;
+    }
+
+    public int priority(){
+        return priority;
+    }
+}
