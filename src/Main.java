b27f841f437cf2bfe7452c8a352d638f93ece619
kaylaato <kaylalistenstomusic@gmail.com>
2026-04-29 17:30:00 +0000

    4/29 added user input/options

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,30 @@ public class Main {
 
         // welcome message
         System.out.println("Welcome to the Assignment Manager 📚");
-        System.out.println("=== Add Assignments ===");
-        boolean done = false;
-        while(done == false){
+
+        // inital setup
+        runInitialSetup(input, manager, myScheduler);
+
+        runMainMenu(input, manager, myScheduler);
+
+
+    }
+
+        public static void runInitialSetup(Scanner input, AssignmentManager manager, Scheduler myScheduler){
+            System.out.println("=== Add Assignments ===");
+            boolean done = false;
+            while(done == false){
+                addSingleAssignment(input, manager, myScheduler);
+
+                System.out.print("Add more assignments? ");
+                String answer = input.nextLine();
+                if(answer.toLowerCase().equals("no")){
+                    done = true;
+                }
+            }
+        }
+
+        private static void addSingleAssignment(Scanner input, AssignmentManager manager, Scheduler myScheduler) {
             System.out.print("Enter assignment name: ");
             String name = input.nextLine();
             System.out.print("Enter due date (format: YYYY-MM-DD): ");
@@ -20,51 +41,52 @@ public class Main {
             input.nextLine();
 
             Assignment a1 = new Assignment(name, due, priority);
+            myScheduler.manager.addAssignment(a1);
+            manager.addAssignmentToGrid(a1);
+        }
 
-            System.out.print("Add more assignments?");
-            String answer = input.nextLine();
-            if(answer.toLowerCase().equals("no")){
-                done = true;
+        public static void runMainMenu(Scanner input, AssignmentManager manager, Scheduler myScheduler) {
+            boolean contin = true;
+            while(contin == true){
+                System.out.println("=== Choose Option ===");
+                System.out.println("1. Edit Assignment | 2. Change View | 3. Suggested Schedule");
+                System.out.print("Enter number: ");
+                int option = input.nextInt();
+                input.nextLine();
+
+                if(option == 1){
+                    System.out.println("1. ");
+                }
+
+                else if(option == 2){
+                    viewOptions(input, manager, myScheduler);
+                }
+
+                else if(option == 3){
+                    showSuggestions(manager, myScheduler);
+                }
+
+                else{
+                    System.out.print("Choice invalid. Enter number: ");
+                    option = input.nextInt();
+                    input.nextLine();
+                }
             }
         }
 
-        // // 1) test adding assignments
-        // Assignment a1 = new Assignment("Math Review", "2026-04-20", 3);
-        // myScheduler.manager.addAssignment(a1);
-        // Assignment a2 = new Assignment("CSA Final Project", "2026-04-15", 4);
-        // myScheduler.manager.addAssignment(a2);
-        // Assignment a3 = new Assignment("Psychology Reading 5.6b", "2026-04-16", 2);
-        // myScheduler.manager.addAssignment(a3);
-        // manager.addAssignmentToGrid(a1);
-        // manager.addAssignmentToGrid(a2);
-        // manager.addAssignmentToGrid(a3);
+        private static void showSuggestions(AssignmentManager manager, Scheduler myScheduler) {
+            System.out.println("=== SUGGESTED START DATE ===");
+            for (Assignment a : myScheduler.manager.getAllAssignments()) {
+                System.out.println(a.getName() + " -> Start by: " + myScheduler.getSuggestedStartDate(a));
+            }
+        }
 
-        // System.out.println("--- INITIAL LIST ---");
-        // System.out.println(manager.toString());
-
-        // // 2) test sorting by priority
-        // System.out.println("--- SORTED BY PRIORITY (Low to High) ---");
-        // manager.sortByPriorityLow();
-        // System.out.println(manager.toString());
-
-        // // 2) test sorting by priority
-        // System.out.println("--- SORTED BY PRIORITY (High to Low) ---");
-        // manager.sortByPriorityHigh();
-        // System.out.println(manager.toString());
-
-        // // 3) test sorting by due date
-        // System.out.println("--- SORTED BY DUE DATE ---");
-        // myScheduler.manager.sortByDueDate();
-        // System.out.println(myScheduler.manager.toString());
-
-        // // 4) test suggested start times
-        // System.out.println("--- SUGGESTED START TIMES ---");
-        // for (Assignment a : myScheduler.manager.getAllAssignments()) {
-        //      System.out.println(a.getName() + " -> Start by: " + myScheduler.getSuggestedStartDate(a));
-        // }
-
-        // // 5) test 2D array implementation
-        // System.out.println(manager.getDashboardString());
-
-    }
+        private static void viewOptions(Scanner input, AssignmentManager manager, Scheduler myScheduler){
+            System.out.println("1. View by Prioirty");
+            System.out.println("2. View by Due Date");
+            System.out.println("3. Show Prioirty Dashboard");
+            System.out.print("Enter number: ");
+            int option2 = input.nextInt();
+            input.nextLine();
+        }
 }
